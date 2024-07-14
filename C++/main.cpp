b476fa662b25904d437fccf861a338d0351fa8fe
Olutoye O <odufowokantoye@outlook.com>
2024-07-13 20:10:03 -0500

    Front end and back end folder created
    Create a C++ back end folder with a temp file and create the front end folder with a html, csss, js temp file

---
+++ C++/main.cpp
@@ -0,0 +1,13 @@
+// Olutoye O
+// odufowokantoye@outlook
+// Date Started: 7-13-24
+// Description: The following project purpose is demostrate a simpilistic version of file encryption and proper c++ security pratices
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
