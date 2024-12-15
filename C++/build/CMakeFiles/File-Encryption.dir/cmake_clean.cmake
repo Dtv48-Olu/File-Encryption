55b9aa8ae16fea72f0112e64b09c3757fd3f346e
Olutoye Odufowokan <odufowokantoye@outlook.com>
2024-12-15 11:10:56 -0600

    Rework the file tree and project to use the cryptopp. Added: Decryption.cpp/h, Encryption.cpp/h, Utils.cpp/h and Removed: Filefiter.h

---
+++ C++/build/CMakeFiles/File-Encryption.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/File-Encryption.dir/main.cpp.o"
+  "CMakeFiles/File-Encryption.dir/main.cpp.o.d"
+  "File-Encryption"
+  "File-Encryption.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/File-Encryption.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/build/CMakeFiles/File-Encryption.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for File-Encryption.
+# This may be replaced when dependencies are built.
